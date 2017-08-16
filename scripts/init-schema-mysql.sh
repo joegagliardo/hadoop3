69ae13ad10af4a3fd121825ce683c3ca5f50613a
joegagliardo <joey@me.com>
2017-08-16 09:26:40 -0400

    1.5.1

--- scripts/init-schema-mysql.sh
+++ scripts/init-schema-mysql.sh
@@ -4,5 +4,6 @@ chown root:root /etc/bootstrap.sh && \
 chmod 700 /etc/bootstrap.sh && \
 cp /usr/local/hive/conf/hive-site-mysql.xml /usr/local/hive/conf/hive-site.xml
 /scripts/start-mysql.sh
+sleep 5
 mysql < /scripts/hiveuser-mysql.sql
 schematool -dbType mysql -initSchema
--- scripts/init-schema-postgres.sh
+++ scripts/init-schema-postgres.sh
@@ -5,5 +5,6 @@ chown root:root /etc/bootstrap.sh && \
 chmod 700 /etc/bootstrap.sh && \
 cp /usr/local/hive/conf/hive-site-postgres.xml /usr/local/hive/conf/hive-site.xml
 /scripts/start-postgres.sh
+sleep 5
 sudo -u postgres psql -f /scripts/hiveuser-postgres.sql
 schematool -dbType postgres -initSchema
